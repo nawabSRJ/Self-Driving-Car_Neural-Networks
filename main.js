0aad0d2a266e6e7eed18209c451a6300e62a3f9d
Srajan Saxena <srajan.saxena7@gmail.com>
2025-06-30 16:03:45 +0530

    road logic done

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2,canvas.width * 0.9);
-const car = new Car(100,100,30,50); // (x,y,width,height)
+// const car = new Car(100,100,30,50); // (x,y,width,height)
+const car = new Car(road.getLaneCenter(1),100,30,50); // dynamically getting the x coord of the car according to the laneCenter function, laneIndex is passed
+
 car.draw(ctx);
 
 animate();
@@ -12,7 +14,12 @@ animate();
 function animate() {
     car.update();
     canvas.height = window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0,-car.y+canvas.height*0.7);
     road.draw(ctx);
     car.draw(ctx);
+
+    ctx.restore();
     requestAnimationFrame(animate); // this calls the animate method again and again many times per second giving the illusion of movement
 }
--- road.js
+++ road.js
@@ -1,29 +1,59 @@
-class Road{
-    constructor(x,width,laneCount=3){
-        this.x=x;
-        this.width=width;
-        this.laneCount=laneCount;
-        this.left=x-width/2;
-        this.right=x+width/2;
+class Road {
+  constructor(x, width, laneCount = 3) {
+    this.x = x;
+    this.width = width;
+    this.laneCount = laneCount;
+    this.left = x - width / 2;
+    this.right = x + width / 2;
 
-        const infinity = 1000000;
-        this.top=-infinity;
-        this.bottom=infinity;
+    const infinity = 1000000;
+    this.top = -infinity;
+    this.bottom = infinity;
+
+    const topLeft={x:this.left,y:this.top};
+    const topRight={x:this.right,y:this.top};
+    const bottomLeft={x:this.left,y:this.bottom};
+    const bottomRight={x:this.right,y:this.bottom};
+    this.borders=[
+        [topLeft,bottomLeft],
+        [topRight,bottomRight],
+    ];
+  }
+
+
+  getLaneCenter(laneIndex){
+    const laneWidth=this.width/this.laneCount;
+    return this.left+laneWidth/2+ Math.min(laneIndex,this.laneCount-1)*laneWidth;
+  }
+
+  draw(ctx) {
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "white";
+
+    for (let i = 1; i <= this.laneCount-1; i++) {
+      const x = lerp(this.left, this.right, i / this.laneCount);
+
+      // setting the dashes in solid lines
+    //   if(i>0 && i < this.laneCount){
+        ctx.setLineDash([20,20]); // [px of length of dash, px of gap b/w dashes]
+    //   }
+    //   else{
+        // ctx.setLineDash([]);
+    //   }
+
+      ctx.beginPath();
+      ctx.moveTo(x, this.top);
+      ctx.lineTo(x, this.bottom);
+      ctx.stroke();
     }
-
-    draw(ctx){
-        ctx.lineWidth=5;
-        ctx.strokeStyle="white";
-        
-        
+    // console.log(this.borders)
+    ctx.setLineDash([]);
+    this.borders.forEach(border=>{
         ctx.beginPath();
-        ctx.moveTo(this.left,this.top);
-        ctx.lineTo(this.left,this.bottom);
+        ctx.moveTo(border[0].x,border[0].y);
+        ctx.lineTo(border[1].x,border[1].y);
         ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(this.right,this.top);
-        ctx.lineTo(this.right,this.bottom);
-        ctx.stroke();
-    }
+    });
+  }
 }
+
